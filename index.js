e9516e99fa22ef10aa123c3a97d0dbfd536c6276
subhyAffara <subhyaffara@gmail.com>
2024-05-26 15:46:13 +0300

    js is still not working

---
+++ index.js
@@ -0,0 +1,15 @@
+function picrotate() {
+
+    for (let i = 1; i < 5; i++) {
+        console.log(i);
+        var f = Math.floor(Math.random() * 4) + 1
+
+        var fuckedup = document.getElementById("#rightpic")
+        fuckedup.innerHTML += `<img src="colomn${f + 1}.jpeg" alt=""`;
+        document.getElementById("#mainsighnup").innerText += ` <img src="colomn${f}.jpeg" alt="">`;
+        document.querySelector("#leftpic").innerHTML += `<img src="colomn${f + 2}.jpeg" alt=""`;
+
+    }
+
+}
+picrotate()
