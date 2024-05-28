770b56f0a41c13d5cb1ecd2fc3877b4721eb53b7
subhyAffara <subhyaffara@gmail.com>
2024-05-28 14:34:49 +0300

    we are gettin close

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 function picrotate() {
 
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i <= 9; i++) {
+        var f = Math.floor(Math.random() * 4) + 1;
         console.log(i);
-        // var f = Math.floor(Math.random() * 4) + 1;
-
-        var fuckedup = document.getElementById("#rightpic")
-        fuckedup.innerHTML += `<img src="colomn${f + 1}.jpeg" alt=""`;
-        document.getElementById("#mainsighnup").innerText += ` <img src="colomn${f}.jpeg" alt="">`;
-        document.querySelector("#leftpic").innerHTML += `<img src="colomn${f + 2}.jpeg" alt=""`;
-        //im trying to put randomized pic in sighnup page
+        console.log(f);
+        document.querySelector("#leftpic").innerHTML += `<img src="colomn${f}.jpeg" alt="">`;
+    }
+    for (let i = 1; i <= 7; i++) {
+        var f = Math.floor(Math.random() * 4) + 1;
+        console.log(i);
+        console.log(f);
+        document.querySelector("#midpic").innerHTML += `<img src="colomn${f}.jpeg" alt="">`;
+    }
+    for (let i = 1; i <= 9; i++) {
+        var f = Math.floor(Math.random() * 4) + 1;
+        console.log(i);
+        console.log(f);
+        document.querySelector("#rightpic").innerHTML += `<img src="colomn${f}.jpeg" alt="">`;
     }
 
 }
@@ -16,16 +24,20 @@ function picrotate() {
 
 function colomnadd() {
 
-    for (let i = 1; i <= 12; i++) {
-        var f = Math.floor(Math.random() * 8) + 1;
+    for (let i = 1; i <= 8; i++) {
+        // var f = Math.floor(Math.random() * 8) + 1;
         console.log(i);
-        console.log(f);
-        document.querySelector("#allproducts").innerHTML += `<div><img src="products${f}.jpeg" alt=""> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis est magni at esse doloribus,
+        // console.log(f);
+        document.querySelector("#allproducts").innerHTML += `<div><img src="products${i}.jpeg" alt=""><div>
+        <h3>available on site </h3>
+        </div> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis est magni at esse doloribus,
         amet aut! Odit repudiandae ducimus aspernatur maxime facilis ad consequuntur.</p></div>`;
     }
 }
-colomnadd()
+// attemt to create a system to make obect and remmeber costumer info in a way where it can stored and revitsed from signup page
 
-// `<img src="products${f}.jpeg" alt="">
-// <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis est magni at esse doloribus,
-//     amet aut! Odit repudiandae ducimus aspernatur maxime facilis ad consequuntur.</p>` <--- this is what im trying to print on products page
+// get a boatload more pics to spice it up
+
+
+
+
