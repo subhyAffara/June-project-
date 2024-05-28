f03a200e96c303d2903c20512dcef0bc6bd282d4
subhyAffara <subhyaffara@gmail.com>
2024-05-28 13:51:46 +0300

    its kinda working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function picrotate() {
 
     for (let i = 1; i < 5; i++) {
         console.log(i);
-        var f = Math.floor(Math.random() * 4) + 1
+        // var f = Math.floor(Math.random() * 4) + 1;
 
         var fuckedup = document.getElementById("#rightpic")
         fuckedup.innerHTML += `<img src="colomn${f + 1}.jpeg" alt=""`;
@@ -12,4 +12,20 @@ function picrotate() {
     }
 
 }
-picrotate()
+
+
+function colomnadd() {
+
+    for (let i = 1; i <= 12; i++) {
+        var f = Math.floor(Math.random() * 8) + 1;
+        console.log(i);
+        console.log(f);
+        document.querySelector("#allproducts").innerHTML += `<div><img src="products${f}.jpeg" alt=""> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis est magni at esse doloribus,
+        amet aut! Odit repudiandae ducimus aspernatur maxime facilis ad consequuntur.</p></div>`;
+    }
+}
+colomnadd()
+
+// `<img src="products${f}.jpeg" alt="">
+// <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae omnis est magni at esse doloribus,
+//     amet aut! Odit repudiandae ducimus aspernatur maxime facilis ad consequuntur.</p>` <--- this is what im trying to print on products page
